d60345cae93a2038411341b909903b88e5b9858e
BERRADA <omar888ber@gmail.com>
2018-12-13 11:45:23 +0100

    agrevid_final_v_3

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,13 @@
-angular.module('MyApp',['appRoutes','mainCtrl','authService','userCtrl','userService','videoCtrl','videoService','playlistCtrl','playlistService','ui.bootstrap'])
+angular.module('MyApp',['appRoutes','mainCtrl','authService','userCtrl','userService','videoCtrl','videoService','playlistCtrl','playlistService','ui.bootstrap','blockUI'])
 
 .config(function($httpProvider){
     $httpProvider.interceptors.push('AuthInterceptor');
+})
+
+.config(function(blockUIConfig){
+    // Change the default overlay message
+    blockUIConfig.message = 'Chargement en cours';
+
+    // Change the default delay to 100ms before the blocking is visible
+    // blockUIConfig.delay = 100;
 });
--- public/app/controllers/videoCtrl.js
+++ public/app/controllers/videoCtrl.js
@@ -1,13 +1,15 @@
 angular.module('videoCtrl', ['videoService'])
 
 
-    .controller('VideoController', function (Video, AuthToken, $window, $sce, $location, $route,$http,$q) {
+    .controller('VideoController', function (Video, AuthToken, $window, $sce, $location, $route,$http,$q,blockUI,$scope,$timeout) {
         let vm = this;
         vm.url = '';
         vm.title = '\xa0\xa0\xa0';
         vm.videoSrc = '';
         vm.image = '';
         vm.youtubeList = true;
+        vm.showVid = false;
+
         // vm.url = "https://r4---sn-n4g-hgnz.googlevideo.com/videoplayback?source=youtube&id=o-AGvY0HursJvaMI26QXpYkeAVD-bDkuc3E5jk7OvhJyEy&requiressl=yes&dur=225.047&pl=23&ip=34.211.145.200&txp=2211222&signature=74D2A92DB76667859A0B0D2E195362DE2B4E33E2.73CEDD01F5C2B33BE3779449A3AF9AC8B6065BD3&ratebypass=yes&fvip=4&ei=EzPQW5nbG5L-kgbQo6GAAw&c=WEB&mime=video%2Fmp4&itag=18&key=cms1&gir=yes&clen=9024154&sparams=clen,dur,ei,expire,gir,id,ip,ipbits,ipbypass,itag,lmt,mime,mip,mm,mn,ms,mv,pcm2cms,pl,ratebypass,requiressl,source&ipbits=0&expire=1540392819&lmt=1535622348334163&redirect_counter=1&rm=sn-nx5ld7z&fexp=23763603&req_id=73294e971bc5a3ee&cms_redirect=yes&ipbypass=yes&mip=78.113.113.141&mm=31&mn=sn-n4g-hgnz&ms=au&mt=1540371087&mv=m&pcm2cms=yes";
 
         vm.searchTitle = Video.getSearchTitle();
@@ -23,6 +25,7 @@ angular.module('videoCtrl', ['videoService'])
 
         //méthode déclanchée lors de la recherche de video
         vm.doSearch = function () {
+            blockUI.start();
 
             Video.searchVimeoVideo(vm.search.title).then(function(res){
                 vm.vimeoVideos = res.data.results;
@@ -51,8 +54,10 @@ angular.module('videoCtrl', ['videoService'])
 
             });
 
-
-
+            $timeout(function() {
+                // Stop the block after some async operation.
+                blockUI.stop();
+            }, 1000);
 
         }
         
@@ -61,7 +66,7 @@ angular.module('videoCtrl', ['videoService'])
             vm.title = video.title;
             vm.videoSrc = 'youtube';
             vm.image = video.raw.snippet.thumbnails.medium.url;
-
+            vm.showVid = true;
             // $location.path('/video');
             // $route.reload();
         }
@@ -71,6 +76,8 @@ angular.module('videoCtrl', ['videoService'])
             vm.title = video.name;
             vm.videoSrc = 'vimeo';
             vm.image = video.pictures.sizes[3].link;
+            vm.showVid = true;
+
             // $location.path('/videoVimeo');
             // $route.reload();
         }
